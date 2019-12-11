fba61fef08d08544d09db455e044f009c55b5fe4
Lucas Jacques <lucas.jacs@gmail.com>
2019-12-11 16:32:53 -0300

    Changed the tests structure + used more "this"

--- 2019/01/main_pt1.js
+++ 2019/01/main_pt1.js
@@ -1,8 +1,6 @@
 const fs = require('fs'),
 	input = fs.readFileSync('data.txt').toString().split('\n'); 
-const myProgram = {
-	logs: { tests: '' },
-};
+const myProgram = {};
 
 myProgram.run = function(data) {
 	let myIndex = 0;
@@ -10,36 +8,23 @@ myProgram.run = function(data) {
 		myIndex++;
 	}
 	myProgram.tests.run();
-	console.log(myProgram.logs.tests);
+	console.log(myProgram.tests.logs);
 }
 
 myProgram.divideAndRoundDown = function(num) {
 	return Math.floor(num / 3);
 }
 
-
-myProgram.test = {};
-myProgram.test.toText = function(result, funcTesting, input, expectation) {
-	if (result) {
-		return `The test of ${funcTesting} function runned smoothly!`;
-	}
-	return `Oops! There was a problem during the test of ${funcTesting} function. We expected ${expectation}, but it resulted in ${myProgram[funcTesting](input)} !`;
-};
-myProgram.test.run = function(funcTesting, input, expectation) {
-	return (myProgram[funcTesting](input) === expectation);
-}
-
-
-myProgram.tests = {};
+myProgram.tests = {logs: ''};
 myProgram.tests.run = function() {
 	const testInputs = {
 		funcTesting: 'divideAndRoundDown',
 	}
 
-	myProgram.logs.tests += "<< RUNNING TESTS >>\n";
+	this.logs += "<< RUNNING TESTS >>\n";
 
 	// tests used on the advent of code
-	myProgram.logs.tests += myProgram.test.toText(myProgram.test.run('divideAndRoundDown', 12, 4), 'divideAndRoundDown', 12, 4) + '\n';
+	this.logs += this.test.toText(this.test.run('divideAndRoundDown', 12, 4), 'divideAndRoundDown', 12, 4) + '\n';
 	// TODO myProgram.divideAndRoundDown(14);
 	// TODO myProgram.divideAndRoundDown(1969);
 	// TODO myProgram.divideAndRoundDown(100756);
@@ -47,9 +32,19 @@ myProgram.tests.run = function() {
 	// TODO // one more for string data
 	// TODO myProgram.divideAndRoundDown('12'));
 	
-	myProgram.logs.tests += "<< TESTS ENDED >>\n";	
+	this.logs += "<< TESTS ENDED >>\n";	
 }
 
+myProgram.tests.test = {};
+myProgram.tests.test.toText = function(result, funcTesting, input, expectation) {
+	if (result) {
+		return `The test of ${funcTesting} function runned smoothly!`;
+	}
+	return `Oops! There was a problem during the test of ${funcTesting} function. We expected ${expectation}, but it resulted in ${myProgram[funcTesting](input)} !`;
+};
+myProgram.tests.test.run = function(funcTesting, input, expectation) {
+	return (myProgram[funcTesting](input) === expectation);
+}
 
 
 
